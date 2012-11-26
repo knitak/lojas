1540eb48e95ab6b6d789e67c77d80c7fba6cd68b
Ana Pedrosa <anacpedrosa@gmail.com>
2012-11-26 15:20:24 +0000

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,14 +3,21 @@ require 'spec_helper'
 describe "StaticPages" do
   describe "Home page" do
 
+  	let(:base_title) { "Recibos Online" }
+
     it "should have the  h1 'Recibos Online'" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text =>'Recibos Online')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "Recibos Online | Home")
+      page.should have_selector('title', :text => "Recibos Online")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
@@ -21,7 +28,7 @@ describe "StaticPages" do
       page.should have_selector('h1', :text =>'Contacts')
     end
 
-    it "should have the title 'Contacts'" do
+    it "should have the title base title" do
       visit '/static_pages/contacts'
       page.should have_selector('title', :text => "Recibos Online | Contacts")
     end
